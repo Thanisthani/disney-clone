d648b94779832ad43bbdf2e38e1fefb58c1555de
Thanistas <thanithanis171198@gmail.com>
2022-04-26 21:14:09 +0530

    Add image folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-   hello
+<Header />
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components';
+function Header() {
+  return (
+    <Container>
+      <Logo src="" />
+    </Container>
+
+  )
+}
+
+
+
+const Container = styled.div`
+height:70px;
+background-color:#090613;
+
+`
+
+const Logo = styled.img`
+height: 50px;
+`
+
+
+export default Header

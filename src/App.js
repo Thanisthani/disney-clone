ff518f33275c4158914667a6dde82bd3ed25a004
Thanistas <thanithanis171198@gmail.com>
2022-04-27 21:59:38 +0530

    Add react route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-
+import Details from './components/Details';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/Details" element={<Details />} />
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
